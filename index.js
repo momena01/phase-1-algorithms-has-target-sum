b05850c8dc64c3832470616bcbdbf47e62820e8d
momena <momena.salloum59@gmail.com>
2023-11-09 23:27:28 +0300

    Done with Assignment

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+    // Write your algorithm here
+    //[3, 8, 12, 4, 11, 7], 10
+    // 0  1  2   3  4   5
+    for (let i = 0; i < array.length; i++) {
+        for (let j = i + 1; j < array.length; j++) {
+            let sum = array[i] + array[j];
+            if (sum === target) {
+                return true;
+            }
+        }
+
+    }
+    return false;
 }
 
 /* 
@@ -8,27 +20,33 @@ function hasTargetSum(array, target) {
 
 /* 
   Add your pseudocode here
+  itreat over array nums 
+    count nums 
+    if sum == target 
+        return true 
+    
 */
 
 /*
   Add written explanation of your solution here
+  write function returns true if there are cuple of numbers in an array their sum == target number 
 */
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
-  // add your own custom tests in here
-  console.log("Expecting: true");
-  console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10));
+    // add your own custom tests in here
+    console.log("Expecting: true");
+    console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10));
 
-  console.log("");
+    console.log("");
 
-  console.log("Expecting: true");
-  console.log("=>", hasTargetSum([22, 19, 4, 6, 30], 25));
+    console.log("Expecting: true");
+    console.log("=>", hasTargetSum([22, 19, 4, 6, 30], 25));
 
-  console.log("");
+    console.log("");
 
-  console.log("Expecting: false");
-  console.log("=>", hasTargetSum([1, 2, 5], 4));
+    console.log("Expecting: false");
+    console.log("=>", hasTargetSum([1, 2, 5], 4));
 }
 
 module.exports = hasTargetSum;
